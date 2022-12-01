afc06ab342d532c5666e789dad277e13d147b1e5
minki3 <86992006+minki3@users.noreply.github.com>
2022-12-01 21:08:52 +0900

    main

---
+++ Frontend/git-test/src/main/main.js
@@ -0,0 +1,11 @@
+export default function Main() {
+  return (
+    <main className="main">
+      <h1>Main</h1>
+      <img className="image" src="images/code.avif" alt="메인 이미지" />
+      <Link to="/">
+        <button className="button">이전으로</button>
+      </Link>
+    </main>
+  );
+}
