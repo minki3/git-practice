2db36e2b9badfc041cf94ba231c10a13c8492fe1
minki3 <86992006+minki3@users.noreply.github.com>
2022-12-01 21:16:01 +0900

    f

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
 
 export default function Router() {
   return (
